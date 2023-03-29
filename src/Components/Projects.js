3028dfa6298e690df476508160b5f278249120d9
Ben De Cruz <decruz_ben@hotmail.com>
2023-03-29 22:41:45 +1100

    Adding link to To Do list(which has yet to be done)

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,7 +4,13 @@ function Projects() {
   return (
     <body className="container">
       <h1 className="display-4 lead">Projects</h1>
-      <p>Projects to be here</p>
+      <a href="https://bendecruz745.github.io/ToDoList/">
+        <h2>To Do List</h2>
+      </a>
+      <p>
+        I've got to start somewhere, and what else other than a simple To Do
+        list that makes me question myself?
+      </p>
     </body>
   );
 }
