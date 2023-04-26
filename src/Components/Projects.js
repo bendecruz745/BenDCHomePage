16cbc4b8ad9b35c5c9e619eeeecdfa4c128c7fd0
Ben De Cruz <decruz_ben@hotmail.com>
2023-04-26 17:16:10 +1000

    Added Weather App portion of Projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,6 +11,15 @@ function Projects() {
         I've got to start somewhere, and what else other than a simple To Do
         list that makes me question myself?
       </p>
+      <a href="https://bendecruz745.github.io/WeatherApp/">
+        <h2>Weather App</h2>
+      </a>
+      <p>
+        Weather App made using WeatherAPI's free API, got the data retrieval and
+        overall functionally going pretty quickly, so I focused this time on
+        making it prettier looking. CSS/Bootstrap may take a bit for me to
+        learn...
+      </p>
     </body>
   );
 }
